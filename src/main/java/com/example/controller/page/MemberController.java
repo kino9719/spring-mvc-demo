ccaeaafc0d1f485c3e49961b5e355aed9a3cf575
kino9719 <kino9719@macbool.local>
2020-07-18 16:39:27 +0800

    add logout api

--- src/main/java/com/example/controller/page/MemberController.java
+++ src/main/java/com/example/controller/page/MemberController.java
@@ -3,10 +3,20 @@ package com.example.controller.page;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class MemberController {
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.removeAttribute("username");
+        session.removeAttribute("isAdmin");
+
+        return "redirect:/login";
     }
 }
