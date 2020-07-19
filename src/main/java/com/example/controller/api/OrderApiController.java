71938ba8466a624c3e8ddf8de5a48a6290a96e56
kino9719 <kino9719@macbool.local>
2020-07-19 12:22:53 +0800

    api block permission

--- src/main/java/com/example/controller/api/OrderApiController.java
+++ src/main/java/com/example/controller/api/OrderApiController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @RestController
@@ -18,12 +19,20 @@ public class OrderApiController {
     OrderService orderService;
 
     @GetMapping("/api/orders/{orderId}/items")
-    public List<OrderItemInfo> items(@PathVariable("orderId") Integer orderId) {
-        return orderService.getOrderItems(orderId);
+    public ResponseEntity<List<OrderItemInfo>> items(@PathVariable("orderId") Integer orderId, HttpSession httpSession) {
+        Integer accountId = (Integer) httpSession.getAttribute("accountId");
+        if (!((Boolean) httpSession.getAttribute("isAdmin")) && !orderService.isAccountOwnTheOrder(accountId, orderId)) {
+            return new ResponseEntity(null, HttpStatus.FORBIDDEN);
+        }
+
+        return new ResponseEntity<List<OrderItemInfo>>(orderService.getOrderItems(orderId), HttpStatus.OK);
     }
 
     @DeleteMapping("/api/orders/{orderId}")
-    public ResponseEntity<String> deleteOrder(@PathVariable("orderId") Integer orderId) {
+    public ResponseEntity<String> deleteOrder(@PathVariable("orderId") Integer orderId, HttpSession httpSession) {
+        if (!((Boolean) httpSession.getAttribute("isAdmin"))) {
+            return new ResponseEntity(null, HttpStatus.FORBIDDEN);
+        }
         orderService.deleteOrder(orderId);
 
         return new ResponseEntity<String>("", HttpStatus.NO_CONTENT);
--- src/main/java/com/example/service/OrderService.java
+++ src/main/java/com/example/service/OrderService.java
@@ -1,6 +1,7 @@
 package com.example.service;
 
 import com.example.model.OrderItemInfo;
+import com.example.model.UserOrder;
 import com.example.repository.OrderItemRepository;
 import com.example.repository.UserOrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OrderService {
@@ -19,6 +21,12 @@ public class OrderService {
 
     public List<OrderItemInfo> getOrderItems(Integer orderId) {
         return userOrderRepository.findOrderItems(orderId);
+    }
+
+    public boolean isAccountOwnTheOrder(Integer accountId, Integer orderId) {
+        Optional<UserOrder> userOrder = userOrderRepository.findById(orderId);
+
+        return userOrder.get() != null && userOrder.get().getAccountId() == accountId;
     }
 
     @Transactional
