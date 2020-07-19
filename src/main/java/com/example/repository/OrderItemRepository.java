a4d2d6db99d394e5271bf0ca8c65c5480e227d32
kino9719 <kino9719@macbool.local>
2020-07-19 12:00:41 +0800

    OrderItemRepository add deleteOrder function

--- src/main/java/com/example/repository/OrderItemRepository.java
+++ src/main/java/com/example/repository/OrderItemRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Integer>, JpaSpecificationExecutor<OrderItem> {
-
+    public void deleteByOrderId(Integer orderId);
 }
--- src/main/java/com/example/service/OrderService.java
+++ src/main/java/com/example/service/OrderService.java
@@ -1,10 +1,12 @@
 package com.example.service;
 
 import com.example.model.OrderItemInfo;
+import com.example.repository.OrderItemRepository;
 import com.example.repository.UserOrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
@@ -12,8 +14,17 @@ public class OrderService {
     @Autowired
     UserOrderRepository userOrderRepository;
 
+    @Autowired
+    OrderItemRepository orderItemRepository;
+
     public List<OrderItemInfo> getOrderItems(Integer orderId) {
         return userOrderRepository.findOrderItems(orderId);
+    }
+
+    @Transactional
+    public void deleteOrder(Integer orderId) {
+        orderItemRepository.deleteByOrderId(orderId);
+        userOrderRepository.deleteById(orderId);
     }
 }
 
