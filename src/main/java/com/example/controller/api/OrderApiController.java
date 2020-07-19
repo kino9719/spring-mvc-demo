16790b1578543ae3870e97137d746f427a208d50
kino9719 <kino9719@macbool.local>
2020-07-19 12:01:01 +0800

    add delete order api

--- src/main/java/com/example/controller/api/OrderApiController.java
+++ src/main/java/com/example/controller/api/OrderApiController.java
@@ -3,6 +3,9 @@ package com.example.controller.api;
 import com.example.model.OrderItemInfo;
 import com.example.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +20,12 @@ public class OrderApiController {
     @GetMapping("/api/order/{orderId}/items")
     public List<OrderItemInfo> items(@PathVariable("orderId") Integer orderId) {
         return orderService.getOrderItems(orderId);
+    }
+
+    @DeleteMapping("/api/order/{orderId}")
+    public ResponseEntity<String> deleteOrder(@PathVariable("orderId") Integer orderId) {
+        orderService.deleteOrder(orderId);
+
+        return new ResponseEntity<String>("", HttpStatus.NO_CONTENT);
     }
 }
